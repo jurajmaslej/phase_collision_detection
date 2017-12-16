bbebfae0a49ef7671c9585f9686f1965ee79175e
juraj <juraj.maslej@gmail.com>
2017-12-16 17:10:32 +0100

    getting closest vertices, problem with updating, cannot reference value from drop or move functions

--- vpython1.py
+++ vpython1.py
@@ -17,6 +17,9 @@ class Obj:
 			self.y = y
 			self.neighbours = []
 			
+		def __str__(self):
+			return 'x: ' + str(self.x) + ' y: ' + str(self.y)
+			
 		def add_neighbourg(self, neighbour):
 			if type(neighbour) == list:
 				for vertex in neighbour:
@@ -34,6 +37,10 @@ class Obj:
 		print('x ',obj.pos.x)
 		print('y ',obj.pos.y)
 		self.obj = obj
+		self.vertices = []
+		
+	def __str__(self):
+		return 'wrapper class for vpython object ' + str(self.obj)
 		
 	def rectangle_points(self, obj):
 		# how the points look like
@@ -44,6 +51,7 @@ class Obj:
 		v2 = self.Vertex(obj.pos.x + obj.length/2, obj.pos.y + obj.height/2)
 		v3 = self.Vertex(obj.pos.x - obj.length/2, obj.pos.y - obj.height/2)
 		v4 = self.Vertex(obj.pos.x + obj.length/2, obj.pos.y - obj.height/2)
+		self.vertices.extend((v1, v2, v3, v4))
 		
 		v1.add_neighbourg([v2, v3])
 		v2.add_neighbourg([v1, v4])
@@ -61,24 +69,84 @@ class Obj:
 		# at the beginning check all vertices
 		# during movement check only neighbours of nearest vertices
 		# still need to implement signed distance for checking distance line-vertex
+		
 		pass
 
 ##scene = display(title= "Phase Collision Detecion", x=0, y=0, width = 800, height = 600, background= color.white)
+objects = []	# store objects
 R1 = box(pos=vector(-5,0,0), size=vector(5,2,0),axis=vector(1,0,0), color=color.yellow)
 R2 = box(pos=vector(5,0,0), size=vector(2,5,0), axis=vector(1,0,0), color=color.yellow)
+
+R1 = Obj(R1)
+R2 = Obj(R2)
+print(R1)
+R1.rectangle_points(R1.obj)
+R2.rectangle_points(R2.obj)
+
+objects.append(R1)
+objects.append(R2)
 
 T = triangle(
           v0=vertex( pos=vec(0,0,0) ),
           v1=vertex( pos=vec(1,0,0) ),
-          v2=vertex( pos=vec(1,1,0) ) )
+          v2=vertex( pos=vec(1,0,0) ),
+          v3=vertex( pos=vec(1,0,0) ),
+          v4=vertex( pos=vec(1,1,0) ) )
 
+# objects.append(T) we assunme only two objects
+
+def closest_vertices():
+	'''
+	at first compare all the vertices
+	possibly slow for multi-vertice objects
+	'''
+	closest_dst = 10000000
+	for v1 in objects[0].vertices:
+		for v2 in objects[1].vertices:
+			v_dst = v1.vertex_dst(v2)
+			if v_dst <= closest_dst:
+				print('closest_dst ', closest_dst)
+				closest_dst = v_dst
+				closest_pair = (v1,v2)
+	print('closest_dst ', closest_dst)
+	print('closest_pair ', closest_pair[0], closest_pair[1])
+	return closest_pair
+
+def new_pair(old_pair):
+	'''
+	should run in move()
+	calculates new closest vertices from neighbours
+	of now closest vertices (ones in old_pair)
+	'''
+	#print('#######################')
+	closest_pair = old_pair
+	v1 = old_pair[0]
+	v2 = old_pair[1]
+	closest = v1.vertex_dst(v2)		#closest dst so far
+	for neigh1 in v1.neighbours:
+		for neigh2 in v2.neighbours:
+			dst = neigh1.vertex_dst(neigh2)
+			if dst < closest:
+				closest = dst
+				closest_pair = (neigh1,neigh2)
+	for neigh1 in v1.neighbours:
+		dst = neigh1.vertex_dst(v2)
+		if dst < closest:
+			closest = dst
+			closest_pair = (neigh1,v2)
+	for neigh2 in v2.neighbours:
+		dst = neigh1.vertex_dst(v1)
+		if dst < closest:
+			closest = dst
+			closest_pair = (v1,neigh2)
+	return closest_pair
+
+vertex_pair = closest_vertices()
+vertex_pair_new = vertex_pair
+print ('new pair ', new_pair(vertex_pair)[0], new_pair(vertex_pair)[1])
 drag_pos = None # No object has been picked yet
 my_object = box(pos=vector(-10,0,0), size=vector(5,2,0),color = color.cyan)
 
-#
-o = Obj(R1)
-o.rectangle_points(R1)
-#
 
 selected = False
 
@@ -87,17 +155,17 @@ def grab(evt):
     print('evt ', evt.event)
     print('scene mouse pick ', scene.mouse.pick)
     print('scene mouse pos ', scene.mouse.pos)
-    if scene.mouse.pick == R1: # if mouseclick on R1, object = R1
+    if scene.mouse.pick == R1.obj: # if mouseclick on R1, object = R1
         drag_pos = evt.pos
         print("SELECTED HERE")
         selected = True
         scene.bind('mousemove', moveR1)
         scene.bind('mouseup', drop)
-    if scene.mouse.pick == R2: # if mouseclick on R2, object =R2
+    if scene.mouse.pick == R2.obj: # if mouseclick on R2, object =R2
         drag_pos = evt.pos
         scene.bind('mousemove', moveR2)
         scene.bind('mouseup', drop)
-
+a = 1
         
 def moveR1(evt):
     global drag_pos
@@ -108,8 +176,9 @@ def moveR1(evt):
         # offset for where the rectangle was touched:
         #print('moving')
         displace = new_pos - drag_pos
-        R1.pos += displace
+        R1.obj.pos += displace
         drag_pos = new_pos # updates drag position
+        
         
 def moveR2(evt):
     global drag_pos
@@ -119,14 +188,18 @@ def moveR2(evt):
     if new_pos != drag_pos: # checks if mouse has moved
         # offset for where the rectangle was touched:
         displace = new_pos - drag_pos
-        R2.pos += displace
+        R2.obj.pos += displace
         drag_pos = new_pos # updates drag position
 
 def drop(evt):
-    scene.unbind('mousemove', moveR1)
-    scene.unbind('mousemove', moveR2)
-    scene.unbind('mouseup', drop)
+	scene.unbind('mousemove', moveR1)
+	scene.unbind('mousemove', moveR2)
+	scene.unbind('mouseup', drop)
+	# do something - label/light closest vertices somehow
+	print(vertex_pair)
+	close = None
+	close = new_pair(vertex_pair)
+	# vertex_pair = close	# error "variable referenced before assingment"
 
 print('start')
-
 scene.bind('mousedown', grab)
